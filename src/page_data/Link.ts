39df247ae414d18f6fe795fbe4d18bf6cfe62b99
JASMIAM <69405366+1498934815@users.noreply.github.com>
2025-09-11 01:14:11 +0800

    更新 Link.ts

--- src/page_data/Link.ts
+++ src/page_data/Link.ts
@@ -1,19 +1,13 @@
 export default {
   // API 接口请求优先，数据格式保持和 data 一致
-  api: 'https://blogapi.jasmiam.dpdns.org/Link.ts',
+  api: '',
   // api 为空则使用 data 静态数据
   data: [
     {
-      "name": "韩小韩博客",
-      "link": "https://www.vvhan.com",
-      "avatar": "https://q1.qlogo.cn/g?b=qq&nk=1655466387&s=640",
-      "descr": "运气是计划之外的东西."
-    },
-    {
-      "name": "韩小韩API",
-      "link": "https://api.vvhan.com",
-      "avatar": "https://api.vvhan.com/static/images/logo.webp",
-      "descr": "免费Web API数据接口调用服务平台."
+      "name": "静谧幻想家博客",
+      "link": "https://jasmiam.top",
+      "avatar": "https://q1.qlogo.cn/g?b=qq&nk=1498934815&s=640",
+      "descr": "除了死亡，一切失败都只存在于心理层面."
     }
   ]
 }
