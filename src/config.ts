eb0c339527f1ac0b5bc8d0002c925a387243b06f
JASM1AM <1498934815@qq.com>
2025-09-18 08:33:29 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -95,7 +95,7 @@ export default {
     // Twikoo 评论
     Twikoo: {
       enable: true,
-      envId: 'https://my-twikoo-one-alpha.vercel.app'
+      envId: 'https://comment.jasmiam.top/'
     },
     // Waline 评论
     Waline: {
