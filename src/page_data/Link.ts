5760f73d4d19a8f337f13aeb31e69427ebd0ace2
JASM1AM <1498934815@qq.com>
2025-09-20 18:21:34 +0800

    更新 Link.ts

--- src/page_data/Link.ts
+++ src/page_data/Link.ts
@@ -1,13 +1,13 @@
 export default {
   // API 接口请求优先，数据格式保持和 data 一致
-  api: 'https://blogapi.jasmiam.top/Link.ts',
+  api: '',
   // api 为空则使用 data 静态数据
   data: [
     {
-      "name": "静谧幻想家博客",
-      "link": "https://jasmiam.top",
-      "avatar": "https://q1.qlogo.cn/g?b=qq&nk=1498934815&s=640",
-      "descr": "除了死亡，一切失败都只存在于心理层面."
+      "name": "网站收录网",
+      "link": "https://www.neacg.cn",
+      "avatar": "https://q1.qlogo.cn/g?b=qq&nk=2787939642&s=640",
+      "descr": "联系QQ：2787939642"
     }
   ]
 }
