42f279674ae7c4518fc788afb6af37283f1c80c7
星觅海 <1498934815@qq.com>
2025-10-24 23:13:02 +0800

    更新 Link.ts

--- src/page_data/Link.ts
+++ src/page_data/Link.ts
@@ -4,27 +4,9 @@ export default {
   // api 为空则使用 data 静态数据
   data: [
     {
-      "name": "网站收录网",
-      "link": "https://www.neacg.cn",
-      "avatar": "https://q1.qlogo.cn/g?b=qq&nk=2787939642&s=640",
-      "descr": "联系QQ：2787939642"
-    },
-    {
       "name": "易搜特收录网",
       "link": "https://www.esoot.com",
-      "avatar": "www.esoot.com/zb_users/theme//JackSite/public/favicon.ico",
-      "descr": ""
-    },
-    {
-      "name": "小鹅导航",
-      "link": "https://www.xedh.cn",
-      "avatar": "www.wxhao.cn/favicon.ico",
-      "descr": ""
-    },
-    {
-      "name": "自动秒收录",
-      "link": "http://www.wxhao.cn/",
-      "avatar": "www.wxhao.cn/favicon.ico",
+      "avatar": "https://esoot.com/zb_users/theme//JackSite/public/favicon.ico",
       "descr": ""
     },
   ]
