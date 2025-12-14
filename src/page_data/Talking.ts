e76d6b6f42d8fce7afd7084f9b20d0722deb3673
星觅海 <1498934815@qq.com>
2025-12-14 09:57:18 +0800

    更新 Talking.ts

--- src/page_data/Talking.ts
+++ src/page_data/Talking.ts
@@ -1,29 +1,15 @@
 export default {
   // API 接口请求优先，数据格式保持和 data 一致
-  api: 'https://blogapi.jasmiam.top/Talking.ts',
+  api: '',
   // api 为空则使用 data 静态数据 
   // 注意：图片请用 vh-img-flex 类包裹
   data: [
     {
-      "data": "2025-09-11 18:45:00",
-      "tags": [
-        "日常"
-      ],
-      "content": "日常<p class=\"ji-img-flex\"><img src=\"https://zycs-img-i4n.pages.dev/v2/0fOSngD.jpeg\"></p>"
-    },
-    {
       "date": "2025-09-10 17:18:00",
       "tags": [
-        "测试"
+        "🌞"
       ],
-      "content": "第二条说说<p class=\"ji-img-flex\"><img src=\"https://q.qlogo.cn/headimg_dl?dst_uin=1498934815&spec=640\"></p>"
-    },
-    {
-      "date": "2025-09-10 17:16:00",
-      "tags": [
-        "测试"
-      ],
-      "content": "第一条说说"
+      "content": "hello<p class=\"ji-img-flex\"><img src=\"https://zycs-img-i4n.pages.dev/v2/0fOSngD.jpeg\"></p>"
     }
-  ]
+]
 }
