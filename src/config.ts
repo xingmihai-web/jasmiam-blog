7bce30f9db3b0074bcaf9de48fedf18553b8d38d
星觅海 <1498934815@qq.com>
2026-06-12 03:07:23 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -34,7 +34,8 @@ export default {
     PageHeight: '28.88rem',
     // 背景
     // https://pan.xmhai.cn/home-banner.webp
-    background: "url('/assets/images/home-banner.webp') no-repeat center 60%/cover",
+    // /assets/images/home-banner.webp
+    background: "url('https://img.xmhai.cn/v2/YEqszPR.jpeg') no-repeat center 60%/cover",
   },
   // 博客主题配置
   Theme: {
